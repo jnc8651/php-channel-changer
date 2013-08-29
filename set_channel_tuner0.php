3d4d5c9ad0827e0103fe629d41d9282969f8a7f1
jnc8651 <jnc8651@gmail.com>
2013-08-29 19:33:01 -0400

    fixed the php exec line to properly grab eth0's ip address and set that as a variable $ip Also added in the ability to check and kill vlc if its running and send the output to /dev/null also the script is echoing out the variables to ensure they have been set however this should not stay this way in production

--- set_channel_tuner0.php
+++ set_channel_tuner0.php
@@ -1,10 +1,17 @@
 <?php
-// set the ip address of the hd homerun prime device 
-$hdrhid = "192.168.3.20";
-//$tuner0vchannel = "12";
-echo exec("ip a show dev eth0 | grep 'inet' | awk \{print $2\} \| awk -F \"/\" '\{print $1\}'");
+// set the ip address of the hd homerun prime device
+$hdrhid = "hdhr-1317135e.vlan3.thelaughingman.local";
+$tuner0vchannel = "12";
+// sets a variable as the ip address of eth0
+$ip = exec("ip a show dev eth0 | grep ' inet ' | awk '{print $2}' | awk -F '/' '{print $1}'");
+//check and see if VLC is running and if so kill the process, if vlc isnt running its not going to complain.
+exec("ps aux | grep [v]lc  | awk '{print $2}' | xargs -L1 kill -9 >/dev/null 2>&1");
+// starts VLC up and sets it to listen to incoming udp traffic on port  5000 and then sets it to transcode and make the stream available on port 8080
+exec("su jnc8651 -c 'cvlc -d udp://@:5000 :sout=#t'ranscode{vcodec=h264,vb=560,fps=30,scale=1,width=1280,height=720,acodec=mp4a,ab=64,channels=2,samplerate=44100,deinterlace}:standard{access=http,mux=flv,dst=:8181/}' :sout-keep'");
 //sets the channel number on tuner 0
-//exec("hdhomerun_config '$hdrhid' set /tuner0/vchannel $tuner0vchannel");
-//echo "$ip";
+exec("hdhomerun_config '$hdrhid' set /tuner0/vchannel $tuner0vchannel");
+// prints out the variables to ensure they are being set right
+echo "eth0 ip is '$ip'";
+echo "\n";
+echo " the hd homerun prime id is '$hdrhid'";
 ?>
-
