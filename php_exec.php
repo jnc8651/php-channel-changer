a7b474995caaf02fdfec3f4740930c892c9f3ef3
jnc8651 <jnc8651@gmail.com>
2013-08-28 22:35:09 -0400

    fixed the variable so it works right and also set the script to echo the output of the php exec command.

--- php_exec.php
+++ php_exec.php
@@ -1,8 +1,8 @@
 <?php
 // outputs the username that owns the running php/httpd process
 // (on a system with the "whoami" executable in the path)
-$HDRHID = '192.168.3.20';
-echo exec('hdhomerun_config $HDHRID get /tuner0/vstatus');
-echo "$HDRHID"
+$hdrhid = "192.168.3.20";
+echo exec("hdhomerun_config '$hdrhid' get /tuner0/vstatus");
+//echo "$hdrhid"
 ?>
 
